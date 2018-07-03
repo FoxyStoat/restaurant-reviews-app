062a1488646424b2e0393560644613f27b1a68f5
FoxyStoat <carol.hodgson@hotmail.com>
2018-07-03 19:33:07 +0100

    feat: Add assets to cache array

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Add version number for easy updating
+const cacheName = 'cache-v1';
+// Files to cache:
+const cacheAssets = [
+ './',
+ './index.html',
+ './restaurant.html',
+ './css/styles.css',
+ './main.js',
+ './data/restaurant.json',
+ './js/dbhelper.js',
+ './js/restaurant_info.js',
+ './img/1.jpg',
+ './img/2.jpg',
+ './img/2.jpg',
+ './img/4.jpg',
+ './img/5.jpg',
+ './img/6.jpg',
+ './img/7.jpg',
+ './img/8.jpg',
+ './img/9.jpg',
+ './img/10.jpg',
+ './img/restaurant_icons.png'
+];
