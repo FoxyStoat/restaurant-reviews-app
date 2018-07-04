529998cf5d38e5db032922aebe3aab1236d1e7d9
FoxyStoat <carol.hodgson@hotmail.com>
2018-07-04 11:30:46 +0100

    fix: Update cache assets array to fix typo

--- sw.js
+++ sw.js
@@ -2,39 +2,39 @@
 const cacheName = 'cache-v1';
 // Files to cache:
 const cacheAssets = [
- './',
- './index.html',
- './restaurant.html',
- './css/styles.css',
- './main.js',
- './data/restaurant.json',
- './js/dbhelper.js',
- './js/restaurant_info.js',
- './img/1.jpg',
- './img/2.jpg',
- './img/2.jpg',
- './img/4.jpg',
- './img/5.jpg',
- './img/6.jpg',
- './img/7.jpg',
- './img/8.jpg',
- './img/9.jpg',
- './img/10.jpg',
- './img/restaurant_icons.png'
+	'./',
+	'./index.html',
+	'./restaurant.html',
+	'./css/styles.css',
+	'./js/main.js',
+	'./data/restaurants.json',
+	'./js/dbhelper.js',
+	'./js/restaurant_info.js',
+	'./img/1.jpg',
+	'./img/2.jpg',
+	'./img/2.jpg',
+	'./img/4.jpg',
+	'./img/5.jpg',
+	'./img/6.jpg',
+	'./img/7.jpg',
+	'./img/8.jpg',
+	'./img/9.jpg',
+	'./img/10.jpg',
+	'./img/restaurant_icons.png'
 ];
 
 /**
- * Install event listener to the service worker, service
- * worker will not install until the code inside waitUntil
- * has sucseeded.
+ * Install and store site assets
+ * Service worker will not install until the code inside
+ * waitUntil has sucseeded.
  */
 self.addEventListener('install', function (evt) {
-  evt.waitUntil(
-    caches
-    .open(cacheName)
-    .then((cache) => {
-      console.log('caching:' + cacheAssets);
-      return cache.addAll(cacheAssets);
-    })
-  );
+	evt.waitUntil(
+		caches
+		.open(cacheName)
+		.then((cache) => {
+			console.log('Service Worker: Caching Assets');
+			return cache.addAll(cacheAssets);
+		})
+	);
 });
